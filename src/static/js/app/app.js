5fe568674d061cf440c9d6cffff4c640c00d651e
Matt Hoffmann Silva <hoffmann.matheus@gmail.com>
2016-04-11 23:31:39 -0400

    Implemented Vue front-end app. Making requests and displaying data on UI.

--- src/static/js/app/app.js
+++ src/static/js/app/app.js
@@ -1,5 +1,4 @@
-
-
+// Change delimiters, since [[]] is used by Flask.
 Vue.config.delimiters = ['[[', ']]'];
 
 var app = new Vue({
@@ -7,16 +6,59 @@ var app = new Vue({
 
   data: {
     queryType: 'coord',
-    url: '/?state=New Jersey'
+    prettyData: '',
+    data: '',
+    isLoading: false,
+    hasError: false
   },
 
   ready: function() {
-    console.log("LOADED");
   },
 
   methods: {
+
     setType: function(type) {
       this.queryType = type;
+    },
+
+    buildUrl: function() {
+      var url = '/?';
+      if (this.queryType == 'coord') {
+        url += 'lat=' + this.coordLat + '&lon=' + this.coordLon;
+      } else {
+        url += 'state=' + this.nameState + '&county=' + this.nameCounty;
+      }
+      return url;
+    },
+
+    search: function() {
+      this.isLoading = true;
+      this.hasError = false;
+
+      $('#buttonCoord').prop('disabled', true);
+      $('#buttonNames').prop('disabled', true);
+
+      var url = this.buildUrl();
+      
+      this.$http({url: url, method: 'GET'}).then(function (response) {
+        this.isLoading = false;
+        this.hasError = false;
+
+        this.prettyData = JSON.stringify(response.data, null, 2);
+
+        $('#buttonCoord').removeProp('disabled');
+        $('#buttonNames').removeProp('disabled');
+
+      }, function (response) { // Error
+        this.isLoading = false;
+        this.hasError = true;
+
+        this.errorMessage = 'Error retrieving data. Status : '+response.status;
+        
+        $('#buttonCoord').removeProp('disabled');
+        $('#buttonNames').removeProp('disabled');
+      });
     }
+
   }
 });
